f9854a4ac99952f6b74bd69fb2b57e6f604eb9a2
Florian Verdonck <florian.verdonck@outlook.com>
2021-04-28 13:56:26 +0000

    Operators

---
+++ src/LanguageFeatures/06-Operators.fs
@@ -0,0 +1,58 @@
+module LanguageFeatures.Operators
+
+open Xunit
+open System
+
+(*
+    In F# it is easy to create custom operators.
+    They can lead to very expressive code.
+    Let's start first with two popular built-in operators: (|>) and (>>)
+    ref: https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/functions/#function-composition-and-pipelining
+*)
+
+[<Fact>]
+let ``meeting the pipe operator`` () =
+    // The pipe operator allows us to pass an expression as last argument of the next function
+    let dateToString (d: DateTime) = d.ToString("dddd")
+    let toUpper (n: string) = n.ToUpper()
+
+    // you need to read this from right to left
+    let expected = toUpper (dateToString DateTime.Now)
+
+    // while this reads from left to right, top to bottom
+    let todayButInCapitals = DateTime.Now |> dateToString |> toUpper
+
+    Assert.Equal(expected, todayButInCapitals)
+
+[<Fact>]
+let ``compose functions`` () =
+    // When the result of a function is the input of another function, those functions can be combined to a new function.
+    // F. ex:
+
+    // DateTime -> string
+    let dateToString (d: DateTime) = d.ToString("dddd")
+    // string -> int
+    let length (n: string) = n.Length
+
+    // DateTime -> int
+    let lengthOfDayName = dateToString >> length
+
+    let expected = length (dateToString DateTime.Now)
+    let actual = lengthOfDayName DateTime.Now
+
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``custom operator`` () =
+    // TODO: create a new operator /+/ that counts a + b + 1
+    // ref: https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/operator-overloading#creating-new-operators
+    let a = 7
+    let b = 2
+    let result = a + b // TODO use custom operator instead of +
+    Assert.Equal(10, result)
+
+[<Fact>]
+let ``assert operator`` () =
+    let a = 5
+    // TODO: create an custom operator ==, that will execute Assert.Equal on two values.
+    Assert.Equal(5, a)
