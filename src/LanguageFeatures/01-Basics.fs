aa5d66127b6cb5774bc218f22ac72f6d6f50215a
nojaf <florian.verdonck@outlook.com>
2021-06-18 14:09:03 +0200

    Add additional pointers for LanguageFeatures.Basics.

--- src/LanguageFeatures/01-Basics.fs
+++ src/LanguageFeatures/01-Basics.fs
@@ -5,11 +5,11 @@ open Xunit // open means using in C#, ref: https://docs.microsoft.com/en-us/dotn
 (*
 
 Hello there, complete the unit tests to learn more about the F# language.
-Some tests will already pass as is, some will need additional code.
+Some tests will already pass as is, some will need additional coding.
 
 The goal is to learn the language feature of F#.
 Sometimes some additional research will be required.
-If that is the case link will provided.
+If that is the case links will provided.
 
 *)
 
@@ -35,6 +35,11 @@ Lastly the () symbol at the end is called unit.
 Unit semantically means the same thing as void in C# however it is a concrete type.
 ref: https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/unit-type
 
+To run this first test, enter the following command in the terminal:
+
+    dotnet test --filter DisplayName~#1.1
+
+make sure you are in the LanguageFeatures folder!
 *)
 [<Fact>]
 let ``#1.1, a first unit test to get things started`` () =
@@ -57,7 +62,20 @@ let ``#1.2, simple if / else structure`` () =
     Assert.Equal(11, length)
 
 [<Fact>]
-let ``#1.3, if / else if / elif / else`` () =
+let ``#1.3, the mutable keyword`` () =
+    // F# is not a pure strict functional language
+    // Since there is interopt with C# you can mutable variables by using the `mutable` keyword.
+    // Avoid this at all time ;)
+    let mutable two = 1
+
+    // TODO: change the value of variable two to 2.
+    // ref: https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/values/#mutable-variables
+    two <- 2
+
+    Assert.Equal(2, two)
+
+[<Fact>]
+let ``#1.4, if / else if / elif / else`` () =
     let numbers = [| 1 .. 100 |] // creates an array with elements from 1 to 100 (boundaries inclusive).
 
     // This is an inner function, scoped to the current test.
@@ -83,6 +101,9 @@ let ``#1.3, if / else if / elif / else`` () =
     // You can assign value to an index with <-, f.ex fizzBuzz.[0] <- "meh"
     let fizzBuzz : string array = Array.zeroCreate 100
 
+    // TODO: iterate over the numbers array to assign each matching value to the fizzBuzz array
+    // Keep in mind that in F# arrays are zero based!
+
     let firstFizz = fizzBuzz.[2]
     let firstBuzz = fizzBuzz.[4]
     let firstFizzBuzz = fizzBuzz.[14]
@@ -90,18 +111,6 @@ let ``#1.3, if / else if / elif / else`` () =
     Assert.Equal("Fizz", firstFizz)
     Assert.Equal("Buzz", firstBuzz)
     Assert.Equal("FizzBuzz", firstFizzBuzz)
-
-[<Fact>]
-let ``#1.4, the mutable keyword`` () =
-    // F# is not a pure strict functional language
-    // Since there is interopt with C# you can mutable variables by using the `mutable` keyword.
-    // Avoid this at all time ;)
-    let mutable two = 1
-
-    // TODO: change the value of variable two to 2.
-    // ref: https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/values/#mutable-variables
-
-    Assert.Equal(2, two)
 
 [<Fact>]
 let ``#1.5, while loops`` () =
@@ -118,9 +127,15 @@ let ``#1.6, first lambda`` () =
     // You can use the `fun` keyword to write lambda's.
     // Mind the -> instead of C#'s =>
     // TODO: complete the lambda so that the value is returned in uppercase.
-    let toUpperCase = fun a -> a
+    // string is an alias for System.String, https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/strings
+    // The string class always has a lot of helper methods out of the box, see https://docs.microsoft.com/en-us/dotnet/api/system.string?view=net-5.0#methods
+    let toUpperCase = fun (a: string) -> a
     // ref: https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/functions/lambda-expressions-the-fun-keyword
 
     let name = "Joey"
     let upperCased = toUpperCase name
     Assert.Equal("JOEY", upperCased)
+
+// Great as you completed the first module, it is a good time to format the F# code.
+// Use the Fantomas tool for this:
+//      dotnet fantomas ./01-Basics.fs
