594c99e66e0f20c308ac06c19e01b233bf37214e
nojaf <florian.verdonck@outlook.com>
2021-06-18 14:23:03 +0200

    Add additional pointers for LanguageFeatures.Tuples.

--- src/LanguageFeatures/02-Tuples.fs
+++ src/LanguageFeatures/02-Tuples.fs
@@ -10,8 +10,8 @@ let ``#2.1, a simple tuple`` () =
     let myTuple = ("John", 77)
     // TODO: replace the values below by the respective values from the tuple.
     // Don't use destructing, keep myTuple as is.
-    let firstPart = "Paul"
-    let secondPart = 75
+    let firstPart = failwith "TODO: implement!" // `failwith` generates an F# exception, https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/exception-handling/the-failwith-function
+    let secondPart = failwith "TODO: implement!" // We want the test to fail at first as there is no implementation provided.
 
     Assert.Equal("John", firstPart)
     Assert.Equal(77, secondPart)
@@ -21,20 +21,22 @@ let ``#2.2, ignoring parts of a tuple`` () =
     // In this sample `year` is not being used.
     // F# tooling should give you a visual clue or a warning about this.
     // TODO: get rid of the warning without changing the right part.
-    let (name, year) = ("Strawberry Fields", 1976)
+    let name, year = ("Strawberry Fields", 1976)
     Assert.Equal("Strawberry Fields", name)
 
 [<Fact>]
 let ``#2.3, C# interop and tuples`` () =
     // Tuple members are separated with `,`.
     // Coming from C# this can appear very confusing.
-    // String.Split take a tuple as argument in this case
+    // String.Split take a tuple as argument in this case, the tuple itself is wrapped in parenthesis.
     let pieces =
         "Penny Lane"
             .Split([| " " |], StringSplitOptions.RemoveEmptyEntries)
 
     Assert.Equal("Penny", pieces.[0])
 
-type Album = (string * int) // We can alias a tuple definition in F#.
-let someAlbum : Album = ("A Hard Day's Night", 1964) // The : Album is not necessary in this case
-// Notice that for tuple definitions we use *, but for assignment we use ,
+[<Fact>]
+let ``#2.4, tuple type definition`` () =
+    // Notice that for tuple definitions we use `*`, but for assignment we use `,`
+    let album : string * int = "A Hard Day's Night", 1964
+    Assert.Equal(("A Hard Day's Night", 1964), album)
